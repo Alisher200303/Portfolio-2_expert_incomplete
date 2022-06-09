b6faa6f474398f1900e6b736665552d06f9baece
Alisher <alishertogoyev682@gmail.com>
2022-06-10 04:58:30 +0500

    Portfolio-2_incomplate

---
+++ JS File/app.js
@@ -0,0 +1,22 @@
+// Navogation bar effects on scroll
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+// Services section - Modal
+
+// Portfolio section - Modal
+
+// Our clients - Swiper
+
+// Scroll to top button
+
+// Navigation menu items active on page scroll
+
+// Responsive navigation menu toggle
+
+// Scroll reveal animations
+
+// Common reveal options to creat reveal animations
+
+// Target elements, and specify options to create reveal animations
